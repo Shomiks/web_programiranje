7f21acc2ea7f49f41040438e5a80ce271e0c8d7b
Shomiks <milos.djurkovic@rbt.rs>
2021-04-16 10:28:59 +0200

    Create input with option to save data

--- src/simpletodo/core.cljs
+++ src/simpletodo/core.cljs
@@ -1,19 +1,41 @@
 (ns simpletodo.core
     (:require
       [reagent.core :as r]
-      [reagent.dom :as d]))
+      [reagent.dom :as rdom]
+      [clojure.string :as str]))
 
 ;; -------------------------
 ;; Views
 
-(defn home-page []
-  [:div [:h2 "Welcome to Reagent"]])
+
+(defn todo-input [{:keys [title on-save on-stop]}]
+  (let [val (r/atom title)
+        stop #(do (reset! val "")
+                  (if on-stop (on-stop)))
+        save #(let [v (-> @val str str/trim)]
+                (if-not (empty? v) (on-save v))
+                (stop))]
+    (fn [{:keys [id class placeholder]}]
+      [:input {:type "text" :value @val
+               :id id :class class :placeholder placeholder
+               :on-blur save
+               :on-change #(reset! val (-> % .-target .-value))
+               :on-key-down #(case (.-which %)
+                               13 (save)
+                               27 (stop)
+                               nil)}])))
+
+(def todo-edit (with-meta todo-input
+                 {:component-did-mount #(.focus (rdom/dom-node %))}))
+
+
+
 
 ;; -------------------------
 ;; Initialize app
 
 (defn mount-root []
-  (d/render [home-page] (.getElementById js/document "app")))
+  (rdom/render [todo-input] (.getElementById js/document "app")))
 
 (defn ^:export init! []
   (mount-root))
