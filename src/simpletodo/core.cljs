37cd3d88fe3732272fb99076b6b3a4b52ffc0a88
Shomiks <milos.djurkovic@rbt.rs>
2021-04-17 20:35:14 +0200

    Add todo-stats and wrap it inside main todo-app component

--- src/simpletodo/core.cljs
+++ src/simpletodo/core.cljs
@@ -7,6 +7,16 @@
 ;; -------------------------
 ;; Views
 
+(defonce todos (r/atom (sorted-map)))
+
+(defn toggle [id] (swap! todos update-in [id :done] not))
+(defn save [id title] (swap! todos assoc-in [id :title] title))
+(defn delete [id] (swap! todos dissoc id))
+
+(defn mmap [m f a] (->> m (f a) (into (empty m))))
+(defn complete-all [v] (swap! todos mmap map #(assoc-in % [1 :done] v)))
+(defn clear-done [] (swap! todos mmap remove #(get-in % [1 :done])))
+
 
 (defn todo-input [{:keys [title on-save on-stop]}]
   (let [val (r/atom title)
@@ -28,14 +38,53 @@
 (def todo-edit (with-meta todo-input
                  {:component-did-mount #(.focus (rdom/dom-node %))}))
 
+(defn todo-stats [{:keys [filt active done]}]
+  (let [props-for (fn [name]
+                    {:class (if (= name @filt) "selected")
+                     :on-click #(reset! filt name)})]
+    [:div
+     [:span#todo-count
+      [:strong active] " " (case active 1 "item" "items") " left"]
+     [:ul#filters
+      [:li [:a (props-for :all) "All"]]
+      [:li [:a (props-for :active) "Active"]]
+      [:li [:a (props-for :done) "Completed"]]]
+     (when (pos? done)
+       [:button#clear-completed {:on-click clear-done}
+        "Clear completed " done])]))
 
+(defn todo-item []
+  (let [editing (r/atom false)]
+    (fn [{:keys [id done title]}]
+      [:li {:class (str (if done "completed ")
+                        (if @editing "editing"))}
+       [:div.view
+        [:input.toggle {:type "checkbox" :checked done
+                        :on-change #(toggle id)}]
+        [:label {:on-double-click #(reset! editing true)} title]
+        [:button.destroy {:on-click #(delete id)}]]
+       (when @editing
+         [todo-edit {:class "edit" :title title
+                     :on-save #(save id %)
+                     :on-stop #(reset! editing false)}])])))
 
+(defn todo-app [props]
+  (let [filt (r/atom :all)]
+    (fn []
+      (let [items (vals @todos)
+            done (->> items (filter :done) count)
+            active (- (count items) done)]
+        [:div
+              [todo-input]
+              [todo-stats {:active active :done done :filt filt}]
+         [:footer#info
+          [:p "Double-click to edit a todo"]]]))))
 
 ;; -------------------------
 ;; Initialize app
 
 (defn mount-root []
-  (rdom/render [todo-input] (.getElementById js/document "app")))
+  (rdom/render [todo-app] (.getElementById js/document "app")))
 
 (defn ^:export init! []
   (mount-root))
