634463559dcdb81c1ccc12ed3d421202d4ade024
Shomiks <milos.djurkovic@rbt.rs>
2021-04-27 17:19:18 +0200

    Complete app functionality and add css to the app

--- src/simpletodo/core.cljs
+++ src/simpletodo/core.cljs
@@ -9,6 +9,12 @@
 
 (defonce todos (r/atom (sorted-map)))
 
+(defonce counter (r/atom 0))
+
+(defn add-todo [text]
+  (let [id (swap! counter inc)]
+    (swap! todos assoc id {:id id :title text :done false})))
+
 (defn toggle [id] (swap! todos update-in [id :done] not))
 (defn save [id title] (swap! todos assoc-in [id :title] title))
 (defn delete [id] (swap! todos dissoc id))
@@ -17,6 +23,12 @@
 (defn complete-all [v] (swap! todos mmap map #(assoc-in % [1 :done] v)))
 (defn clear-done [] (swap! todos mmap remove #(get-in % [1 :done])))
 
+(defonce init (do
+                (add-todo "Kupi hleb, jogurt, mleko")
+                (add-todo "Idi u teretanu")
+                (add-todo "Zavrsi sve taskove na poslu")
+                (add-todo "Izadji uvece na pice")
+                (complete-all true)))
 
 (defn todo-input [{:keys [title on-save on-stop]}]
   (let [val (r/atom title)
@@ -75,10 +87,26 @@
             done (->> items (filter :done) count)
             active (- (count items) done)]
         [:div
-              [todo-input]
-              [todo-stats {:active active :done done :filt filt}]
-         [:footer#info
-          [:p "Double-click to edit a todo"]]]))))
+         [:section#todoapp
+          [:header#header
+           [:h1 "todos"]
+           [todo-input {:id "new-todo"
+                        :placeholder "What needs to be done?"
+                        :on-save add-todo}]]
+          (when (-> items count pos?)
+            [:div
+             [:section#main
+              [:input#toggle-all {:type "checkbox" :checked (zero? active)
+                                  :on-change #(complete-all (pos? active))}]
+              [:ul#todo-list
+               (for [todo (filter (case @filt
+                                    :active (complement :done)
+                                    :done :done
+                                    :all identity) items)]
+                 ^{:key (:id todo)} [todo-item todo])]]
+             [:footer#footer
+              [todo-stats {:active active :done done :filt filt}]]])]
+         [:footer#info]]))))
 
 
 ;; -------------------------
